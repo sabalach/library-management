b39b3dda6e614e4636213c65c82da4585a87077c
Sabal Ach <2038sabal@gmail.com>
2021-08-28 21:46:35 +0545

    Stats fix

--- server/graphql/resolvers/stats.js
+++ server/graphql/resolvers/stats.js
@@ -6,10 +6,10 @@ module.exports = {
   Query: {
     async getStats() {
       const totalBooks = await Book.count({ deleted: false });
-      const oldBooks = await Book.count({ condition: 'OLD' });
-      const newBooks = await Book.count({ condition: 'NEW' });
-      const damagedBooks = await Book.count({ condition: 'DAMAGED' });
-      const lostBooks = await Book.count({ condition: 'LOST' });
+      const oldBooks = await Book.count({ condition: 'OLD', deleted: false });
+      const newBooks = await Book.count({ condition: 'NEW', deleted: false });
+      const damagedBooks = await Book.count({ condition: 'DAMAGED', deleted: false });
+      const lostBooks = await Book.count({ condition: 'LOST', deleted: false });
       const totalStudents = await Student.count({ deleted: false });
       const borrowedBooks = await BookLog.count({ returnedDate: null });
       return {
