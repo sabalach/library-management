b4d8fd4b38c12af41019e7d6c772164e5ffede54
Sabal Ach <2038sabal@gmail.com>
2021-08-31 20:03:30 +0545

    Book Sn Search

--- src/components/BookTable/index.js
+++ src/components/BookTable/index.js
@@ -77,6 +77,44 @@ const columns = [
     title: 'Serial Number',
     dataIndex: 'serialNumber',
     key: 'serialNumber',
+    filterDropdown: ({
+      setSelectedKeys, selectedKeys, confirm, clearFilters,
+    }) => (
+      <div style={{ padding: 8 }}>
+        <Input
+          placeholder="Search Serial Number"
+          value={selectedKeys[0]}
+          onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          style={{ width: 188, marginBottom: 8, display: 'block' }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => {
+              confirm();
+            }}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => {
+              clearFilters();
+            }}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Reset
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: filtered => <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />,
+    onFilter: (value, record) => (record.serialNumber
+      ? record.serialNumber.toLowerCase().includes(value.toLowerCase())
+      : ''),
   },
   {
     title: 'Action',
